e5f783ea5a8551688fe4760577dfcf487da27100
Ben Krikler <b.krikler@cern.ch>
2017-06-10 15:46:23 +0100

    Add core interface

---
+++ core/table_completion.py
@@ -0,0 +1,32 @@
+import alphatwirl
+
+def complete(df_configs, completer):
+        df_configs = [completer.complete(c) for c in df_configs]
+        reader_collector_pairs = [build_counter_collector_pair(config) for config in df_configs]
+        return reader_collector_pairs
+
+def build_counter_collector_pair(df_cfg):
+    keyValComposer = alphatwirl.summary.KeyValueComposer(
+        keyAttrNames = df_cfg['keyAttrNames'],
+        binnings = df_cfg['binnings'],
+        keyIndices = df_cfg['keyIndices'],
+        valAttrNames = df_cfg['valAttrNames'],
+        valIndices = df_cfg['valIndices']
+    )
+    nextKeyComposer = alphatwirl.summary.NextKeyComposer(df_cfg['binnings']) if df_cfg['binnings'] is not None else None
+    summarizer = alphatwirl.summary.Summarizer(
+        Summary = df_cfg['summaryClass']
+    )
+    reader = alphatwirl.summary.Reader(
+        keyValComposer = keyValComposer,
+        summarizer = summarizer,
+        nextKeyComposer = nextKeyComposer,
+        weightCalculator = df_cfg['weight'],
+        nevents = df_cfg['nevents']
+    )
+    resultsCombinationMethod = alphatwirl.collector.ToDataFrame(
+        summaryColumnNames = df_cfg['keyOutColumnNames'] + df_cfg['valOutColumnNames']
+    )
+    deliveryMethod = None
+    collector = alphatwirl.loop.Collector(resultsCombinationMethod, deliveryMethod)
+    return reader, collector
