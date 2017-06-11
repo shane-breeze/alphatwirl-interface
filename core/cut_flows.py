eb52f6c1b391141712dd4c661252fe537126da6b
Ben Krikler <b.krikler@cern.ch>
2017-06-12 00:56:33 +0100

    Add cut_flow wrapper

---
+++ core/cut_flows.py
@@ -0,0 +1,15 @@
+import alphatwirl
+
+def cut_flow_with_counter(cut_flow, cut_flow_summary_filename):
+    eventSelection = alphatwirl.selection.build_selection(
+        path_cfg = cut_flow,
+        AllClass = alphatwirl.selection.modules.AllwCount,
+        AnyClass = alphatwirl.selection.modules.AnywCount,
+        NotClass = alphatwirl.selection.modules.NotwCount
+    )
+    resultsCombinationMethod = alphatwirl.collector.ToTupleListWithDatasetColumn(
+        summaryColumnNames = ('depth', 'class', 'name', 'pass', 'total')
+    )
+    deliveryMethod = alphatwirl.collector.WriteListToFile(cut_flow_summary_filename)
+    collector = alphatwirl.loop.Collector(resultsCombinationMethod, deliveryMethod)
+    return eventSelection, collector
